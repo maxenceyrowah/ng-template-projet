e90f0ec37156fda35ac0f8658503633d76eb46b9
max <max@incubtek.com>
2024-02-19 13:12:35 +0000

    fix routing of app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/components/layout/layout.component';
 
 export const routes: Routes = [
-  { path: '', component: LayoutComponent, pathMatch: 'full' },
+  { path: 'dashboard', component: LayoutComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
